74f8490af55baf2993b88f36e1134ae37a8348b1
Parth Sarin <parthsarin@gmail.com>
2016-02-21 23:23:54 -0600

    add frontend progress

--- src/InventoryBundle/Controller/MainController.php
+++ src/InventoryBundle/Controller/MainController.php
@@ -35,4 +35,31 @@ class MainController extends Controller
 
       return compact('items', 'profit', 'percent_profit');
     }
+
+    /**
+     * @Route("/progress")
+     * @Template
+     */
+    public function progressAction()
+    {
+      $em = $this->getDoctrine()->getManager();
+      $items_unparsed = $em->getRepository('InventoryBundle:Item')->findAll();
+
+      $profit = floatval(0);
+      $items = array();
+      foreach ($items_unparsed as $item) {
+        $profit = $profit - floatval($item->getCostAcquisition()) * floatval($item->getInitialQuantity());
+        $profit = $profit + floatval($item->getPrice()) * floatval($item->getSold());
+
+        if ($item->getPrice() != 0)
+        {
+          array_push($items, $item);
+        }
+      }
+      $profit = $profit + 5.94; # adjustment
+      $projected_profit = 2374.57;
+      $percent_profit = intval(($profit / $projected_profit) * 100);
+
+      return compact('items', 'profit', 'percent_profit');
+    }
 }
